1c8f2f8bc32e65e9d148f5164a4a571c19167b29
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2022-02-21 20:30:06 +0300

    refactor component Rating

--- src/Components/Rating/Rating.tsx
+++ src/Components/Rating/Rating.tsx
@@ -1,40 +1,27 @@
 import React from 'react';
-import {Star, StarBorder} from "@material-ui/icons";
+import {Star} from "@material-ui/icons";
 
 
 
 export const Rating = () => {
 
-    const [activeStar, setActiveStar] = React.useState<number>(0);
+    const starState: number[] = [1, 2, 3, 4, 5];
 
-    const onClickHandler = (num: number) => {
-        activeStar === num
-            ? setActiveStar(num - 1)
-            : setActiveStar(num);
-    }
+    const [active, setActive] = React.useState<number>(0);
 
     return (
         <div>
-            <Star fontSize="large"
-                  onClick={() => onClickHandler(1)}
-                  className={activeStar > 0 ? 'star' : ''}
-            />
-            <Star fontSize="large"
-                  onClick={() => onClickHandler(2)}
-                  className={activeStar > 1 ? 'star' : ''}
-            />
-            <Star fontSize="large"
-                  onClick={() => onClickHandler(3)}
-                  className={activeStar > 2 ? 'star' : ''}
-            />
-            <Star fontSize="large"
-                  onClick={() => onClickHandler(4)}
-                  className={activeStar > 3 ? 'star' : ''}
-            />
-            <Star fontSize="large"
-                  onClick={() => onClickHandler(5)}
-                  className={activeStar > 4 ? 'star' : ''}
-            />
+            {
+                starState.map((s, i) => {
+                    return (
+                      <Star key={i}
+                            fontSize={'large'}
+                            onClick={() => setActive(i + 1)}
+                            className={active > i ? 'starBg' : ''}
+                      />
+                    );
+                })
+            }
         </div>
     );
 };
